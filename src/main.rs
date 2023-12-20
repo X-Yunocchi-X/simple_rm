49e979b93c03feabd9f4fc70e3b9ec1f7dae75c3
X-Yunocchi-X <syf2003_bool@163.com>
2023-12-20 22:28:58 +0800

    update

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,11 @@
 mod simple_rm;
 
-use std::{
-    env,
-    path::{Path, PathBuf},
-};
+use std::env;
 
 use simple_rm::SimpleRemove;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
-    let rm = SimpleRemove::new(PathBuf::from("/home/yuno/.local/share/Trash"), args);
-    rm.remove_file(Path::new("/home/yuno/Downloads/1.txt"));
+    let rm = SimpleRemove::new(args);
+    rm.execute();
 }
--- src/simple_rm.rs
+++ src/simple_rm.rs
@@ -1,6 +1,10 @@
 use std::{
+    env,
+    ffi::OsStr,
     fs::{self, File},
-    path::{Path, PathBuf}, io::{self, Write},
+    io::{self, Write},
+    path::{Path, PathBuf},
+    time::UNIX_EPOCH,
 };
 
 /// To remove file to trash bin, need to create file.trashinfo in $trash/info/
@@ -14,20 +18,47 @@ pub struct SimpleRemove {
 // Usage:
 // simple_rm -rf <file>
 impl SimpleRemove {
-    pub fn new(trash_path: PathBuf, args: Vec<String>) -> Self {
-        return SimpleRemove { trash_path, args };
+    pub fn new(args: Vec<String>) -> Self {
+        let home = env::var("HOME").unwrap();
+        let trash_path = PathBuf::from(format!("{}/.local/share/Trash", home));
+        SimpleRemove { trash_path, args }
+    }
+
+    pub fn execute(&self) {
+        self.args.iter().skip(1).for_each(|arg| {
+            if arg.starts_with("-") {
+                // -r(f)
+                todo!()
+            } else {
+                let file_path = Path::new(arg);
+                self.remove_file(file_path);
+            }
+        })
     }
 
     pub fn remove_file(&self, file_path: &Path) {
-        self._remove_file(file_path).unwrap();
-        self.create_info(file_path).unwrap();
+        if file_path.is_dir() {
+            self.create_info(file_path).unwrap();
+            self.remove_dir(file_path);
+        } else {
+            self.move_file_to_trash(file_path).unwrap();
+            self.create_info(file_path).unwrap();
+        }
+        // self.synchronize();
+    }
+
+    fn get_absolute_path(file_path: &Path) -> io::Result<Path> {
+        if file_path.is_absolute() {
+            return Ok(file_path);
+        } else {
+            let pwd = env::current_dir()?;
+            return Ok(&pwd.join(file_path));
+        }
     }
 
     /// Remove file to trash bin
-    fn _remove_file(&self, file_path: &Path) -> io::Result<()> {
-        let file_name = file_path
-            .file_name()
-            .expect("Can not get filename, maybe the filename has invalid utf-8 characters");
+    fn move_file_to_trash(&self, file_path: &Path) -> io::Result<()> {
+        let file_name = Self::get_file_name(file_path);
         let trash_dir = self.trash_path.join("files");
         fs::rename(file_path, trash_dir.join(file_name))?;
         Ok(())
@@ -35,16 +66,49 @@ impl SimpleRemove {
 
     fn create_info(&self, file_path: &Path) -> io::Result<()> {
         let info_dir = self.trash_path.join("info");
-        let file_name = file_path
-            .file_name()
-            .expect("Can not get filename, maybe the filename has invalid utf-8 characters");
-        let extended_name = format!("{}.trashinfo", file_name.to_str().expect("Can not convert filename to utf-8"));
+        let file_name = Self::get_file_name(file_path);
+        let extended_name = format!(
+            "{}.trashinfo",
+            file_name
+                .to_str()
+                .expect("Can not convert filename to utf-8")
+        );
         let mut info_file = File::create(info_dir.join(extended_name))?;
-        let time = chrono::Local::now();
-        let format_time = time.format("%Y-%m-%dT%H:%M:%S").to_string();
-        let info_content = format!("[Trash Info]\nPath={}\nDeletionDate={}", file_path.display(), format_time);
+        let time = chrono::Local::now().format("%Y-%m-%dT%H:%M:%S").to_string();
+        let info_content = format!(
+            "[Trash Info]\nPath={}\nDeletionDate={}",
+            file_path.display(),
+            time
+        );
         info_file.write_all(info_content.as_bytes())?;
-        println!("{}", info_content);
+        // println!("{}", info_content);
         Ok(())
+    }
+
+    /// Create directory cache in $trash/directorysizes
+    /// format: [size] [mtime] [name] mtime is the time of last modification
+    fn create_dir_cache(&self) {
+        todo!()
+    }
+
+    fn get_modified_time(file_path: &Path) -> io::Result<u64> {
+        let modified_time = fs::metadata(file_path)?.modified()?;
+        let duration_since_epoch = modified_time
+            .duration_since(UNIX_EPOCH)
+            .expect("time is before UNIX epoch");
+        Ok(duration_since_epoch.as_secs())
+    }
+
+    fn get_file_name(path: &Path) -> &OsStr {
+        path.file_name()
+            .expect("Can not get filename, maybe the filename has invalid utf-8 characters")
+    }
+
+    fn remove_dir(&self, file_path: &Path) -> ! {
+        todo!()
+    }
+
+    fn synchronize(&self) -> ! {
+        todo!()
     }
 }
